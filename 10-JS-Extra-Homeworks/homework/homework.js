cf6487397adcfa5097f721f759c6133499d35847
rosameza63 <mezarosalba4@gmail.com>
2022-08-24 22:46:44 -0500

    Ultimo ejercicio

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -9,7 +9,15 @@ function deObjetoAmatriz(objeto){
       B: 2,
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
-  //Escribe tu código aquí
+  var matriz1=[];
+  
+  
+  for (let clave in objeto){
+    
+    matriz1.push([clave,objeto[clave]]);
+    
+  }
+  return matriz1;
 }
 
 
@@ -18,6 +26,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let cuenta = {};
+
+  for(let i = 0; i < string.length; i++){
+    if(!cuenta[string[i]]){
+      cuenta[string[i]] = 0;
+    }  
+    cuenta[string[i]] += 1;
+  }
+  return cuenta;
 }
 
 
@@ -25,7 +42,23 @@ function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
-  //Escribe tu código aquí
+ 
+    var palabraMayuscula=''; 
+    var palabraMinuscula='';
+    var nuevo=s;
+    for(var i = 0; i < nuevo.length; i++){
+      var letra = nuevo[i];
+      if(letra == letra.toUpperCase()){
+        //Si entra, significa que es mayuscula
+        palabraMayuscula+= letra; //Concatenamos la letra mayuscula
+      }else{
+        palabraMinuscula+= letra; //Concatenamos la letra minuscula
+      }
+  }
+  //Al terminar el ciclo, concatenamos ambos resultados y lo retornamos
+  palabra=palabraMayuscula + palabraMinuscula;
+  return palabra;
+
 }
 
 
@@ -34,7 +67,19 @@ function asAmirror(str) {
   //Escribe una función que tome la frase recibida y la devuelva de modo tal que se pueda leer de izquierda a derecha 
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
-  //Escribe tu código aquí
+   var cadena= str.split(' ');
+   var cadenatotal='';
+   var largo=cadena.length-1;
+   for(i=0;i< cadena.length; i++){
+    var reverso=cadena[i].split('').reverse().join('');
+     if (i === largo){
+      cadenatotal=cadenatotal+reverso;
+     }
+     else{
+      cadenatotal=cadenatotal+reverso+' ';
+     }
+    }
+   return cadenatotal;
 } 
 
 
@@ -42,7 +87,15 @@ function capicua(numero){
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
-  //Escribe tu código aquí
+  var numerostr=''+numero;
+  var reverso=numerostr.split('').reverse().join('');
+  if (numerostr == reverso){
+    return 'Es capicua';
+  }
+  else{
+    return 'No es capicua';
+  }
+
 }
 
 
@@ -50,13 +103,35 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var palabranueva='';
+
+    var nuevo=cadena;
+    for(var i = 0; i < nuevo.length; i++){
+      var letra = nuevo[i];
+      if(letra === 'a' || letra === 'b' || letra === 'c'){
+          palabranueva+= '';
+      }else{
+        palabranueva+= letra; 
+      }
+  }
+
+    return palabranueva;
+  
+  
 }
 
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
-  //Escribe tu código aquí
+ 
+  arr.sort(function(a, b) {
+    //Los queremos ordenados del mas corto al mas largo, por lo tanto usamos...
+    return a.length - b.length
+    //En caso contrario, usariamos return b.length - a.length
+  });
+ return arr;
+  
 }
 
 
@@ -65,7 +140,9 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  var common = arreglo1.filter(x => arreglo2.includes(x))
+ 
+  return common;
 }
 
 
